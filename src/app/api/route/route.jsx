2d3bab8bcd08c17439a9810410aade2773001cc7
yujin <kingyujin56@gmail.com>
2025-01-16 13:57:57 +0900

    1.16

--- src/app/api/route/route.jsx
+++ src/app/api/route/route.jsx
@@ -25,38 +25,34 @@ const EmailContent = (data) => {
   };
 };
 
+
 const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'kingyujin56@gmail.com',
-    pass: 'irmd xbzt hyqb nysp',
+    user: process.env.EMAIL_USER, 
+    pass: process.env.EMAIL_PASS, 
   },
 });
 
-// 받는 사람 등 기본 메일 옵션
 const mailOptions = {
-  from: 'kingyujin56@gmail.com', 
-  to: '97o912@naver.com',        
+  from: process.env.EMAIL_USER,
+  to: process.env.EMAIL_TO, 
 };
 
 export async function POST(request) {
   try {
-    // 1) 폼 데이터 파싱
     const { name, email, subject, message } = await request.json();
     console.log({ name, email, subject, message });
 
-    // 2) 본문 생성
     const { text, html } = EmailContent({ name, email, subject, message });
 
-    // 3) nodemailer로 메일 전송
     await transporter.sendMail({
       ...mailOptions,
-      subject,     // 사용자가 입력한 제목
-      text,        // 폼 데이터를 text 형태로
-      html,        // 폼 데이터를 html 형태로
+      subject,
+      text,
+      html,
     });
 
-    // 메일 전송 성공 시
     return NextResponse.json({ message: 'Form submitted successfully' });
   } catch (error) {
     console.error(error);
